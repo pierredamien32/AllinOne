0d85ca93f43f61ee51dd4acac6a98fd4fdab6757
OrpheeAlitcha <orpheetohou@alitchateam.com>
2023-03-27 12:47:43 +0100

    Confirguration personnel de mon style tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,26 @@ module.exports = {
     "./resources/**/*.vue",
   ],
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: ['Inter', 'sans-serif']
+    },
+    borderRadius: {
+      'none': '0',
+      'sm': '0.125rem',
+      'default': '0.25rem',
+      'lg': '0.5rem',
+      'full': '9999px',
+      '32': '32px',
+      '40': '40px',
+      '30': '30px',
+      '20': '20px',
+      '10': '10px',
+    },
+    extend: {
+      backgroundImage: {
+        'hero-pattern': "url('/public/images/image de la page d'accueil.png')",
+      }
+    },
   },
   plugins: [],
 }
